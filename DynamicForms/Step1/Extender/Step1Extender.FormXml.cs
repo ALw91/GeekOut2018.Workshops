cb520b2db68e55a1f49c0a44295467fb68da8ca6
Robert Szatkowski <robert.szatkowski@enova.pl>
2018-05-04 19:09:09 +0200

    Stworzenie poziomego i pionowego układu kontrolek w ramach Page

--- DynamicForms/Step1/Extender/Step1Extender.FormXml.cs
+++ DynamicForms/Step1/Extender/Step1Extender.FormXml.cs
@@ -84,6 +84,70 @@ namespace DynamicForms.Step1 {
         private UIElement GetVerticalElements() {
             if (stackVertical == null) {
                 stackVertical = new StackContainer();
+
+                var group0 = new GroupContainer {
+                    Name = "groupV0",
+                    CaptionHtml = "Element typu GroupContainer - układ pionowy",
+                    Width = "*",
+                    Height = "400px"
+                };
+
+                var stack1 = new StackContainer {
+                    LabelWidth = "20"
+                };
+                var stack2 = new StackContainer();
+
+                var label1 = new LabelElement {
+                    Name = "label1V",
+                    CaptionHtml = "Nazwa"
+                };
+                var field1 = new FieldElement {
+                    Name = "fieldV1",
+                    CaptionHtml = "",
+                    EditValue = "{Operator.Name}",
+                    Width = "35"
+                };
+
+                var label2 = new LabelElement {
+                    Name = "label2V",
+                    CaptionHtml = "Pełna nazwa"
+                };
+                var field2 = new FieldElement {
+                    Name = "fieldV2",
+                    CaptionHtml = "",
+                    EditValue = "{Operator.FullName}",
+                    Width = "35"
+                };
+
+                var field3 = new FieldElement {
+                    Name = "fieldV3",
+                    CaptionHtml = "Standardowy operator",
+                    EditValue = "{Operator.IsStandard}",
+                    Width = "8",
+                    OuterWidth = "37"
+                };
+                var field4 = new FieldElement {
+                    Name = "fieldV4",
+                    CaptionHtml = "",
+                    EditValue = "{Image}",
+
+                    Class = new UIClass[] { UIClass.ImageEdit },
+                    Width = "200px",
+                    Height = "200px"
+                };
+
+                stack1.Elements.Add(label1);
+                stack1.Elements.Add(field1);
+
+                stack1.Elements.Add(label2);
+                stack1.Elements.Add(field2);
+
+                stack1.Elements.Add(field3);
+                stack2.Elements.Add(field4);
+
+                group0.Elements.Add(stack2);
+                group0.Elements.Add(stack1);
+                stackVertical.Elements.Add(group0);
             }
             return stackVertical;
         }
