2b01c24c4a199f1b7209333054025d90ef80c6e6
Robert Szatkowski <rs2065@hotmail.com>
2018-05-04 18:58:42 +0200

    Dynamic forms step0 after work (#5)
    Stworzenie poziomego układu kontrolek w ramach Page

---
+++ DynamicForms/Step0/Extender/Extender.FormXml.cs
@@ -0,0 +1,66 @@
+﻿using Soneta.Business.UI;
+
+namespace DynamicForms.Step0 {
+    public partial class Extender {
+        private StackContainer stackRoot;
+
+        public UIElement GetStackElement() {
+            if (stackRoot == null) {
+                stackRoot = new StackContainer();
+                var group0 = new GroupContainer {
+                    Name = "group0",
+                    CaptionHtml = "Element typu GroupContainer, układ poziomy",
+                    Width = "*",
+                    Height = "220px"
+                };
+
+                var row = new RowContainer();
+
+                var stack1 = new StackContainer {
+                    LabelWidth = "20"
+                };
+                var stack2 = new StackContainer();
+
+                var field1 = new FieldElement {
+                    Name = "fieldH1",
+                    CaptionHtml = "Nazwa",
+                    EditValue = "{Operator.Name}",
+                    Width = "30"
+                };
+                var field2 = new FieldElement {
+                    Name = "fieldH2",
+                    CaptionHtml = "Pełna nazwa",
+                    EditValue = "{Operator.FullName}",
+                    Width = "30"
+                };
+                var field3 = new FieldElement {
+                    Name = "fieldH3",
+                    CaptionHtml = "Standardowy operator",
+                    EditValue = "{Operator.IsStandard}",
+                    Width = "8"
+                };
+                var field4 = new FieldElement {
+                    Name = "fieldH4",
+                    CaptionHtml = "",
+                    EditValue = "{Image}",
+
+                    Class = new UIClass[] { UIClass.ImageEdit },
+                    Width = "200px",
+                    Height = "200px"
+                };
+
+                stack1.Elements.Add(field1);
+                stack1.Elements.Add(field2);
+                stack1.Elements.Add(field3);
+                stack2.Elements.Add(field4);
+
+                row.Elements.Add(stack2);
+                row.Elements.Add(stack1);
+
+                group0.Elements.Add(row);
+                stackRoot.Elements.Add(group0);
+            }
+            return stackRoot;
+        }
+    }
+}
---
+++ DynamicForms/Step0/Extender/Extender.cs
@@ -0,0 +1,15 @@
+﻿using Soneta.Business;
+using Soneta.Business.App;
+
+[assembly: Worker(typeof(DynamicForms.Step0.Extender))]
+
+namespace DynamicForms.Step0 {
+    public partial class Extender : ContextBase {
+        public Extender(Context cx) : base(cx) {
+        }
+
+        public Operator Operator => Context.Login.Operator;
+
+        public NamedStream Image  => Operator.DefaultImage;
+    }
+}
