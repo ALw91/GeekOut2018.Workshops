13e4b258de3df8d32d53c83e6058a00b18af3f76
Robert Szatkowski <rs2065@hotmail.com>
2018-05-09 10:12:26 +0200

    Dynamic forms step2 before (#7)
    Zainicjowanie projektu - Krok 2

---
+++ DynamicForms/Step2/Extender/Step2Extender.FormXml.cs
@@ -0,0 +1,25 @@
+﻿using Soneta.Business.UI;
+
+namespace DynamicForms.Step2.Extender {
+    public partial class Step2Extender {
+        private GroupContainer root;
+
+        public UIElement GetStackElement() {
+            checkBuffer();
+            if (root == null) {
+                root = new GroupContainer {
+                    CaptionHtml = "Generowana lista spotkań",
+                    Height = "*",
+                    Width = "*"
+                };
+
+                root.Elements.Add(new GapElement {
+                    Height = "5",
+                    Width = "*"
+                });
+            }
+            return root;
+        }
+
+    }
+}
---
+++ DynamicForms/Step2/Extender/Step2Extender.cs
@@ -0,0 +1,26 @@
+﻿using DynamicForms.Step2.Business;
+using Soneta.Business;
+
+[assembly: Worker(typeof(DynamicForms.Step2.Extender.Step2Extender))]
+
+namespace DynamicForms.Step2.Extender {
+    public partial class Step2Extender : ContextBase {
+
+        private Params _parameters;
+
+        public Step2Extender(Context cx) : base(cx) {
+        }
+
+        public Params FilterParameters => _parameters ?? (_parameters = new Params(Context));
+
+        public TaskProxy[] Tasks {
+            get {
+                checkBuffer();
+                if (tasks == null) {
+                    RefreshTasks();
+                }
+                return tasks;
+            }
+        }
+    }
+}
