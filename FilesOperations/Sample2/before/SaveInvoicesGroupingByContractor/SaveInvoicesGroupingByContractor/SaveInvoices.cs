725181cbf53a6ba8a19d5c7b79d5ba50368314db
Anna Łuczyńska <anna.luczynska@enova.pl>
2018-05-11 12:41:02 +0200

    Sample2 / Before / IFileSystemService / WriteFile

---
+++ FilesOperations/Sample2/before/SaveInvoicesGroupingByContractor/SaveInvoicesGroupingByContractor/SaveInvoices.cs
@@ -0,0 +1,22 @@
+﻿using SaveInvoicesGroupingByContractor;
+using Soneta.Business;
+using Soneta.Magazyny;
+
+[assembly: Worker(typeof(SaveInvoices), typeof(Obrot))]
+
+namespace SaveInvoicesGroupingByContractor
+{
+    public class SaveInvoices
+    {
+
+        [Action("Zapisz faktury",
+            Target = ActionTarget.ToolbarWithText | ActionTarget.Menu | ActionTarget.LocalMenu | ActionTarget.Divider,
+            Mode = ActionMode.SingleSession)]
+        public object SaveDataWorker()
+        {
+            return null;
+        }
+
+    }
+
+}
