edc1cb6aa4238c0ef78389061826c750f62a50f2
Anna Łuczyńska <anna.luczynska@enova.pl>
2018-05-11 12:54:25 +0200

    Sample3 / Before / IFileSystemService / ReadFile

---
+++ FilesOperations/Sample3/before/ReadFileFromList/ReadFileFromList/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReadFileFromList.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReadFileFromList.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa8a1ec1-38cb-4c27-b760-a67d48fcf421")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FilesOperations/Sample3/before/ReadFileFromList/ReadFileFromList/Workers/ReadFile.cs
@@ -0,0 +1,22 @@
+﻿using ReadFileFromList.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+
+[assembly: Worker(typeof(ReadFile), typeof(DokHandlowe))]
+
+namespace ReadFileFromList.Workers
+{
+    public class ReadFile
+    {
+
+        [Action("Odczytaj dane",
+            Target = ActionTarget.ToolbarWithText | ActionTarget.Menu | ActionTarget.LocalMenu | ActionTarget.Divider,
+            Mode = ActionMode.SingleSession)]
+        public object ReadDataWorker()
+        {
+            return null;
+        }
+
+    }
+
+}
